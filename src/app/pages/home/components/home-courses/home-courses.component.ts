752bbe3858b80044c8ee907e2626c3ef4db443a9
mahmoud mabrouk <mahmoud.mabrouk@vultara.com>
2024-10-20 20:32:08 +0300

    add swiper

--- src/app/pages/home/components/home-courses/home-courses.component.ts
+++ src/app/pages/home/components/home-courses/home-courses.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
+import { register } from 'swiper/element/bundle';
 
+register();
 @Component({
   standalone: true,
   selector: 'app-home-courses',
   templateUrl: './home-courses.component.html',
   styleUrls: ['./home-courses.component.css'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomeCoursesComponent implements OnInit {
   constructor() {}
